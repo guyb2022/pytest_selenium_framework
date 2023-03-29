1f3751dd4413ffa264026408d4f5fd4c11ea38ce
Guy B <91497242+guyb2022@users.noreply.github.com>
2023-03-29 15:48:58 +0300

    add html report, optional browser to select from

---
+++ README.txt
@@ -0,0 +1,19 @@
+Folder structure of project:
+Last updated: 29/03/2023
+
+configurations
+  - config.ini
+Logs
+  - automation.log
+Reports
+Screenshots
+testCases
+  - __init__.py
+  - conftest.py
+  - test_login.py
+TestData
+utilities
+  - __init__.py
+  - customLogger.py
+  - readProperties.py
+requirements.txt
--- testCases/conftest.py
+++ testCases/conftest.py
@@ -2,6 +2,32 @@ from selenium import webdriver
 import pytest
 
 @pytest.fixture()
-def setup():
-    driver=webdriver.Chrome()
+def setup(browser):
+    if browser == "chrome":
+        driver=webdriver.Chrome()
+    elif browser == 'firefox':
+        driver=webdriver.Firefox()
+    else:
+        driver=webdriver.Ie()
     return driver
+
+def pytest_addoption(parser):
+    # get the value from the cli/ hooks
+    parser.addoption("--browser")
+
+@pytest.fixture()
+def browser(request):
+    # return the Browser value to setup method
+    return request.config.getoption("--browser")
+
+############# pytest html reports ###################
+def pytest_configure(config):
+    config._metadata['Project Name'] = 'Intel Workday'
+    config._metadata['Module Name'] = 'Users'
+    config._metadata['Tester'] = 'G.B Tester'
+
+@pytest.mark.optionalhook
+def pytest_metatada(metadata):
+    pass
+    # metadata.pop("somthing", None)
+####################################################
